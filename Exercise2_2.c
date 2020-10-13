f835c1e274abd12fab16576e6d50c9d2dbdc2aec
Felix Liu <felixliu@kth.se>
2020-10-13 09:43:09 +0200

    Removed debug print

--- Exercise2_2.c
+++ Exercise2_2.c
@@ -18,7 +18,6 @@ int main(int argc, char* argv[])
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    printf("Rank %d says hello!\n", rank);
 
     int count = 0;
     double x, y, z, pi;
